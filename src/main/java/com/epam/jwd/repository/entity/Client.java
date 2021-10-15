f30460d30ff88eaa1e01b0bbc53bcf5ce06cd1ab
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-10-16 01:37:17 +0300

    Create Client entity class

---
+++ src/main/java/com/epam/jwd/repository/entity/Client.java
@@ -0,0 +1,60 @@
+package com.epam.jwd.repository.entity;
+
+import java.util.Objects;
+
+public class Client extends AbstractEntity<Integer> {
+
+    private String username;
+    private String password;
+
+    public Client() {
+    }
+
+    public Client(Integer id) {
+        super(id);
+    }
+
+    public Client(Integer id, String username, String password) {
+        super(id);
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return username.equals(client.username)
+                && password.equals(client.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
