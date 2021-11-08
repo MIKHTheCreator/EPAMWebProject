5938ac53a9e0e4324576277f6eec4afdea281245
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-11-08 14:35:04 +0300

    Refactor code

--- src/main/java/com/epam/jwd/dao/message/SQLConfig.java
+++ src/main/java/com/epam/jwd/dao/message/SQLConfig.java
@@ -5,6 +5,6 @@ public interface SQLConfig {
     Integer INITIAL_SIZE = 4;
     String SQL_DB_URL = "jdbc:mysql://localhost:3306/banksystem";
     String SQL_DB_USERNAME = "mikh";
-    String SQL_DB_PASSWORD = "*652296D2A&C798A9C5053Ec5A72Ebf93BB026B8C";
+    String SQL_DB_PASSWORD = "*652296D2A7C798A9C5053Ec5A72Ebf93BB026B8C";
     String SQL_DB_DRIVER = "com.jdbc.mysql.Driver";
 }
