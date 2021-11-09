7611c49a8bf394dcae3abf004b3d9ed11cd2db3e
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-11-09 15:27:52 +0300

    Create controller class

---
+++ src/main/java/com/epam/jwd/controller/ApplicationController.java
@@ -0,0 +1,33 @@
+package com.epam.jwd.controller;
+
+import com.epam.jwd.controller.command.Command;
+import com.epam.jwd.controller.command.ResponseContext;
+import com.epam.jwd.controller.request_context.impl.RequestContextImpl;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ApplicationController extends HttpServlet {
+
+    private static final String COMMAND_PARAMETER = "command";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String commandName = request.getParameter(COMMAND_PARAMETER);
+        Command command = Command.of(commandName);
+        ResponseContext commandResult = command.execute(new RequestContextImpl(request));
+
+        if(commandResult.isRedirect()) {
+            response.sendRedirect(commandResult.getPage());
+        } else {
+            RequestDispatcher dispatcher = request.getRequestDispatcher(commandResult.getPage());
+            dispatcher.forward(request, response);
+        }
+
+    }
+}
