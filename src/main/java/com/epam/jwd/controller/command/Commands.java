cb05694f6ceec29fa120ef53a4a19a868b9488a3
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-11-12 15:56:21 +0300

    Refactor code

--- src/main/java/com/epam/jwd/controller/command/Commands.java
+++ src/main/java/com/epam/jwd/controller/command/Commands.java
@@ -1,20 +1,13 @@
 package com.epam.jwd.controller.command;
 
 import com.epam.jwd.controller.command.impl.DefaultCommand;
-import com.epam.jwd.controller.command.impl.ErrorCommand;
 import com.epam.jwd.controller.command.impl.ShowUsersCommand;
 
 import java.util.Arrays;
 
 public enum Commands {
     DEFAULT(DefaultCommand.getInstance()),
-    ERROR(ErrorCommand.getInstance()),
-    SHOW_USERS(ShowUsersCommand.getInstance()),
-    BLOCK_USER(BlockUserCommand.getInstance()),
-    SHOW_CREDIT_CARDS(ShowCreditCardsCommand.getInstance()),
-    DELETE_CREDIT_CARD(DeleteCreditCardCommand.getInstance()),
-    SHOW_PAYMENTS(ShowPaymentsCommand.getInstance()),
-    SHOW_PASSPORT_DATA(ShowPassportDataCommand.getInstance());
+    SHOW_USERS(ShowUsersCommand.getInstance());
 
     private final Command command;
 
