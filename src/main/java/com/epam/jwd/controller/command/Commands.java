41942606fd928207debb0f3d865b0aae12cde4fc
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-11-13 15:29:16 +0300

    Add SAVE_USER_COMMAND field to Commands

--- src/main/java/com/epam/jwd/controller/command/Commands.java
+++ src/main/java/com/epam/jwd/controller/command/Commands.java
@@ -2,13 +2,14 @@ package com.epam.jwd.controller.command;
 
 import com.epam.jwd.controller.command.impl.DefaultCommand;
 import com.epam.jwd.controller.command.impl.SaveClientCommand;
+import com.epam.jwd.controller.command.impl.SaveUserCommand;
 
 import java.util.Arrays;
 
 public enum Commands {
     DEFAULT(DefaultCommand.getInstance()),
-    SAVE_CLIENT_COMMAND(SaveClientCommand.getInstance());
-
+    SAVE_CLIENT_COMMAND(SaveClientCommand.getInstance()),
+    SAVE_USER_COMMAND(SaveUserCommand.getInstance());
 
     private final Command command;
 
