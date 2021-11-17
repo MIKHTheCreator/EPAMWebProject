d6703c477488a7c0c710d3d3812219dff28b36bc
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-11-18 00:47:10 +0300

    Create class

---
+++ src/main/java/com/epam/jwd/controller/command/impl/show_page/ShowEditUserInfoPageCommand.java
@@ -0,0 +1,32 @@
+package com.epam.jwd.controller.command.impl.show_page;
+
+import com.epam.jwd.controller.command.Command;
+import com.epam.jwd.controller.command.ResponseContext;
+import com.epam.jwd.controller.request_context.RequestContext;
+
+public class ShowEditUserInfoPageCommand implements Command {
+
+    private static final Command INSTANCE = new ShowEditUserInfoPageCommand();
+    private static final String PAGE_PATH = "/WEB-INF/jsp/edit_user_info.jsp";
+
+    private static final ResponseContext SHOW_EDIT_USER_INFO_PAGE_CONTEXT = new ResponseContext() {
+        @Override
+        public String getPage() {
+            return PAGE_PATH;
+        }
+
+        @Override
+        public boolean isRedirect() {
+            return true;
+        }
+    };
+
+    public static Command getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public ResponseContext execute(RequestContext context) {
+        return SHOW_EDIT_USER_INFO_PAGE_CONTEXT;
+    }
+}
