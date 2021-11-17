bd1c6ff919e64f95eaaae60f5999979382890331
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-11-18 00:46:31 +0300

    Edit Commands

--- src/main/java/com/epam/jwd/controller/command/Commands.java
+++ src/main/java/com/epam/jwd/controller/command/Commands.java
@@ -1,6 +1,9 @@
 package com.epam.jwd.controller.command;
 
 import com.epam.jwd.controller.command.impl.*;
+import com.epam.jwd.controller.command.impl.show_page.ShowErrorPageCommand;
+import com.epam.jwd.controller.command.impl.show_page.ShowLogInPageCommand;
+import com.epam.jwd.controller.command.impl.show_page.ShowRegistrationPageCommand;
 import com.epam.jwd.dao.entity.user_account.Role;
 
 import java.util.Arrays;
@@ -8,15 +11,16 @@ import java.util.List;
 
 public enum Commands {
     DEFAULT(DefaultCommand.getInstance()),
-    SAVE_CLIENT_COMMAND(SaveClientCommand.getInstance()),
-    SAVE_USER_COMMAND(SaveUserCommand.getInstance()),
-    EDIT_USER_COMMAND(EditUserCommand.getInstance()),
-    SAVE_PASSPORT_COMMAND(SavePassportCommand.getInstance()),
-    LOG_OUT_USER_COMMAND(LogOutUserCommand.getInstance()),
+    SAVE_CLIENT_COMMAND(SaveClientCommand.getInstance(), Role.UNAUTHORIZED),
+    SAVE_USER_COMMAND(SaveUserCommand.getInstance(), Role.UNAUTHORIZED),
+    EDIT_USER_COMMAND(EditUserCommand.getInstance(), Role.USER, Role.ADMIN),
+    SAVE_PASSPORT_COMMAND(SavePassportCommand.getInstance(), Role.USER, Role.ADMIN),
+    LOG_OUT_USER_COMMAND(LogOutUserCommand.getInstance(), Role.USER, Role.ADMIN),
     CHANGE_LANGUAGE_COMMAND(ChangeLanguageCommand.getInstance()),
-    AUTHORIZATION_COMMAND(AuthorisationCommand.getInstance()),
-    SHOW_LOGIN_PAGE_COMMAND(ShowLogInPageCommand.getInstance()),
-    SHOW_REGISTRATION_PAGE_COMMAND(ShowRegistrationPageCommand.getInstance());
+    AUTHORIZATION_COMMAND(AuthorisationCommand.getInstance(), Role.UNAUTHORIZED),
+    SHOW_LOGIN_PAGE_COMMAND(ShowLogInPageCommand.getInstance(), Role.UNAUTHORIZED),
+    SHOW_REGISTRATION_PAGE_COMMAND(ShowRegistrationPageCommand.getInstance(), Role.UNAUTHORIZED),
+    SHOW_ERROR_PAGE_COMMAND(ShowErrorPageCommand.getInstance());
 
     private final Command command;
     private final List<Role> allowedRoles;
