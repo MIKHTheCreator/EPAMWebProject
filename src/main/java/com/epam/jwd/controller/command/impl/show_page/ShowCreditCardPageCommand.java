a49a80e065382cbe056dcbff386fc1f8fd0126ed
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-11-19 03:05:37 +0300

    Create ShowCreditCardPageCommand

---
+++ src/main/java/com/epam/jwd/controller/command/impl/show_page/ShowCreditCardPageCommand.java
@@ -0,0 +1,32 @@
+package com.epam.jwd.controller.command.impl.show_page;
+
+import com.epam.jwd.controller.command.Command;
+import com.epam.jwd.controller.command.ResponseContext;
+import com.epam.jwd.controller.request_context.RequestContext;
+
+public class ShowCreditCardPageCommand implements Command {
+
+    private static final Command INSTANCE = new ShowCreditCardPageCommand();
+    private static final String PAGE_PATH = "/WEB-INF/jsp/credit_cards.jsp";
+
+    private static final ResponseContext SHOW_CREDIT_CARD_CONTEXT = new ResponseContext() {
+        @Override
+        public String getPage() {
+            return PAGE_PATH;
+        }
+
+        @Override
+        public boolean isRedirect() {
+            return true;
+        }
+    };
+
+    public static Command getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public ResponseContext execute(RequestContext context) {
+        return SHOW_CREDIT_CARD_CONTEXT;
+    }
+}
