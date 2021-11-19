6b65e4531b14c2b4fffbd97166524e7a8a171603
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-11-20 00:03:31 +0300

    Create ShowUsersPageCommand

---
+++ src/main/java/com/epam/jwd/controller/command/impl/show_page/ShowUsersPageCommand.java
@@ -0,0 +1,32 @@
+package com.epam.jwd.controller.command.impl.show_page;
+
+import com.epam.jwd.controller.command.Command;
+import com.epam.jwd.controller.command.ResponseContext;
+import com.epam.jwd.controller.request_context.RequestContext;
+
+public class ShowUsersPageCommand implements Command {
+
+    private static final Command INSTANCE = new ShowUsersPageCommand();
+    private static final String PAGE_PATH = "/WEB-INF/jsp/users.jsp";
+
+    private static final ResponseContext SHOW_USERS_PAGE_CONTEXT = new ResponseContext() {
+        @Override
+        public String getPage() {
+            return PAGE_PATH;
+        }
+
+        @Override
+        public boolean isRedirect() {
+            return true;
+        }
+    };
+
+    public static Command getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public ResponseContext execute(RequestContext context) {
+        return SHOW_USERS_PAGE_CONTEXT;
+    }
+}
