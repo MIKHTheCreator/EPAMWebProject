60cd3a007a01b4d057384307e706d5c15728991b
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-11-25 00:00:16 +0300

    Refactor code

--- src/main/java/com/epam/jwd/dao/message/ClientDAOMessage.java
+++ src/main/java/com/epam/jwd/dao/message/ClientDAOMessage.java
@@ -6,7 +6,7 @@ public interface ClientDAOMessage {
             "VALUES (?, ?, ?)";
     String SQL_FIND_ALL_CLIENTS_QUERY = "SELECT (client_id, username, email, password) FROM client";
     String SQL_FIND_BY_ID_CLIENT_QUERY = "SELECT (client_id, username, email, password) FROM client WHERE client_id=?";
-    String SQL_FIND_BY_USERNAME_CLIENT_QUERY = "SELECT (client_id, username, email, password) FROM client WHERE username=?";
+    String SQL_FIND_BY_USERNAME_CLIENT_QUERY = "SELECT client_id, username, email, password FROM `client` WHERE username=?";
     String SQL_UPDATE_CLIENT_QUERY = "UPDATE client SET username=? email=? passport=? WHERE client_id=?";
     String SQL_DELETE_CLIENT_QUERY = "DELETE FROM client WHERE client_id=?";
 }
--- src/main/java/com/epam/jwd/service/impl/user_account/ClientService.java
+++ src/main/java/com/epam/jwd/service/impl/user_account/ClientService.java
@@ -12,6 +12,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static com.epam.jwd.dao.message.ExceptionMessage.DELIMITER;
