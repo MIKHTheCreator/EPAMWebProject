9e1a41bbe30c58ebe15c031f1d3eae44ae26a149
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-11-26 03:12:39 +0300

    Refactor code

--- src/main/java/com/epam/jwd/dao/message/CreditCardDAOMessage.java
+++ src/main/java/com/epam/jwd/dao/message/CreditCardDAOMessage.java
@@ -2,15 +2,10 @@ package com.epam.jwd.dao.message;
 
 public interface CreditCardDAOMessage {
 
-    String SQL_SAVE_CREDIT_CARD_QUERY = "INSERT INTO credit_card (number, expiration_date," +
-            " full_name, cvv, pin, user_id, bank_account_id) VALUES (?, ?, ?, ?, ?, ?, ?)";
-    String SQL_FIND_ALL_CREDIT_CARDS_QUERY = "SELECT credit_card_id, number, expiration_date," +
-            " full_name, cvv, pin, user_id, bank_account_id FROM credit_card";
-    String SQL_FIND_ALL_CREDIT_CARDS_BY_USER_ID_QUERY = "SELECT credit_card_id, number, expiration_date," +
-            " full_name, cvv, pin, user_id, bank_account_id FROM credit_card WHERE user_id=?";
-    String SQL_FIND_CREDIT_CARD_BY_ID_QUERY = "SELECT credit_card_id, number, expiration_date," +
-            " full_name, cvv, pin, user_id, bank_account_id FROM credit_card WHERE credit_card_id=?";
-    String SQL_UPDATE_CREDIT_CARD_QUERY = "UPDATE credit_card SET number=? expiration=?" +
-            " full_name=? cvv=? pin=? user_id=? bank_account_id =? WHERE credit_card_id=?";
+    String SQL_SAVE_CREDIT_CARD_QUERY = "INSERT INTO credit_card (number, expiration_date, full_name, cvv, pin, user_id, bank_account_id) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    String SQL_FIND_ALL_CREDIT_CARDS_QUERY = "SELECT credit_card_id, number, expiration_date, full_name, cvv, pin, user_id, bank_account_id FROM credit_card";
+    String SQL_FIND_ALL_CREDIT_CARDS_BY_USER_ID_QUERY = "SELECT credit_card_id, number, expiration_date, full_name, cvv, pin, user_id, bank_account_id FROM credit_card WHERE user_id=?";
+    String SQL_FIND_CREDIT_CARD_BY_ID_QUERY = "SELECT credit_card_id, number, expiration_date, full_name, cvv, pin, user_id, bank_account_id FROM credit_card WHERE credit_card_id=?";
+    String SQL_UPDATE_CREDIT_CARD_QUERY = "UPDATE credit_card SET number=?, expiration=?, full_name=?, cvv=?, pin=?, user_id=?, bank_account_id =? WHERE credit_card_id=?";
     String SQL_DELETE_CREDIT_CARD_QUERY = "DELETE FROM credit_card WHERE credit_card_id=?";
 }
--- src/main/java/com/epam/jwd/dao/message/PaymentDAOMessage.java
+++ src/main/java/com/epam/jwd/dao/message/PaymentDAOMessage.java
@@ -2,12 +2,10 @@ package com.epam.jwd.dao.message;
 
 public interface PaymentDAOMessage {
 
-    String SQL_SAVE_PAYMENT_QUERY = "INSERT INTO payment (sum, date," +
-            " organization, goal, bank_account_id, user_id) VALUES (?, ?, ?, ?, ?, ?)";
+    String SQL_SAVE_PAYMENT_QUERY = "INSERT INTO payment (sum, date, organization, goal, bank_account_id, user_id) VALUES (?, ?, ?, ?, ?, ?)";
     String SQL_FIND_ALL_PAYMENTS_QUERY = "SELECT payment_id, sum, date, organization, goal, bank_account_id, user_id FROM payment";
     String SQL_FIND_ALL_PAYMENTS_BY_USER_ID_QUERY = "SELECT payment_id, sum, date, organization, goal, bank_account_id, user_id FROM payment WHERE payment_id=id";
     String SQL_FIND_PAYMENT_BY_ID_QUERY = "SELECT payment_id, sum, date, organization, goal, bank_account_id, user_id FROM payment WHERE payment_id=?";
-    String SQL_UPDATE_PAYMENT_QUERY = "UPDATE payment SET sum=? date=?" +
-            "organization=? goal=? bank_account_id=? user_id=? WHERE payment_id=?";
+    String SQL_UPDATE_PAYMENT_QUERY = "UPDATE payment SET sum=?, date=?, organization=?, goal=?, bank_account_id=?, user_id=? WHERE payment_id=?";
     String SQL_DELETE_QUERY = "DELETE FROM payment WHERE payment_id=?";
 }
