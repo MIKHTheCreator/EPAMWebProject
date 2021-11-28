b04320c88b170b971811e0416b42dd3c64d5e293
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-11-29 00:12:43 +0300

    Add command

--- src/main/java/com/epam/jwd/controller/command/impl/ShowUsersCommand.java
+++ src/main/java/com/epam/jwd/controller/command/impl/ShowUsersCommand.java
@@ -3,11 +3,26 @@ package com.epam.jwd.controller.command.impl;
 import com.epam.jwd.controller.command.Command;
 import com.epam.jwd.controller.command.ResponseContext;
 import com.epam.jwd.controller.request_context.RequestContext;
+import com.epam.jwd.service.api.UserService;
+import com.epam.jwd.service.dto.user_account.UserDTO;
+import com.epam.jwd.service.exception.ServiceException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 public class ShowUsersCommand implements Command {
 
+    private final UserService<UserDTO, Integer> userService = new com.epam.jwd.service.impl.user_account.UserService();
     private static final Command INSTANCE = new ShowUsersCommand();
     private static final String PAGE_PATH = "/WEB-INF/jsp/users.jsp";
+    private static final String ERROR_PAGE_PATH = "/WEB-INF/jsp/error.jsp";
+    private static final String USERS_ATTRIBUTE = "users";
+    private static final String ERROR_ATTRIBUTE = "error";
+    private static final String ERROR_MESSAGE = "Can't find users";
+
+    private static final Logger log = LogManager.getLogger(ShowUsersCommand.class);
 
     private static final ResponseContext SHOW_USERS_CONTEXT = new ResponseContext() {
         @Override
@@ -17,7 +32,19 @@ public class ShowUsersCommand implements Command {
 
         @Override
         public boolean isRedirect() {
-            return true;
+            return false;
+        }
+    };
+
+    private static final ResponseContext ERROR_CONTEXT = new ResponseContext() {
+        @Override
+        public String getPage() {
+            return ERROR_PAGE_PATH;
+        }
+
+        @Override
+        public boolean isRedirect() {
+            return false;
         }
     };
 
@@ -27,6 +54,23 @@ public class ShowUsersCommand implements Command {
 
     @Override
     public ResponseContext execute(RequestContext context) {
+
+        HttpSession session;
+
+        if(context.getCurrentSession().isPresent()) {
+            session = context.getCurrentSession().get();
+        } else {
+            return ERROR_CONTEXT;
+        }
+
+        try {
+            List<UserDTO> users = userService.findAll();
+            session.setAttribute(USERS_ATTRIBUTE, users);
+        } catch (ServiceException e) {
+            log.error(ERROR_MESSAGE, e);
+            context.addAttributeToJsp(ERROR_ATTRIBUTE, ERROR_MESSAGE + e.getMessage());
+        }
+
         return SHOW_USERS_CONTEXT;
     }
 }
