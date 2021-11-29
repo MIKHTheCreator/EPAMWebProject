d7c22f2b4a8d34fd01dc552bb51d52a4fabad807
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-11-30 00:21:31 +0300

    Refactor code

--- src/main/java/com/epam/jwd/controller/command/impl/ShowUsersCreditCardsCommand.java
+++ src/main/java/com/epam/jwd/controller/command/impl/ShowUsersCreditCardsCommand.java
@@ -1,6 +1,7 @@
 package com.epam.jwd.controller.command.impl;
 
 import com.epam.jwd.controller.command.Command;
+import com.epam.jwd.controller.command.response_context.ErrorResponseContext;
 import com.epam.jwd.controller.command.response_context.ResponseContext;
 import com.epam.jwd.controller.request_context.RequestContext;
 import com.epam.jwd.service.dto.payment_system.CreditCardDTO;
@@ -17,7 +18,6 @@ public class ShowUsersCreditCardsCommand implements Command {
     private final CreditCardService creditCardService = new CreditCardService();
     private static final Command INSTANCE = new ShowUsersCreditCardsCommand();
     private static final String PAGE_PATH = "/WEB-INF/jsp/credit_cards.jsp";
-    private static final String ERROR_PATH = "/WEB-INF/jsp/error.jsp";
     private static final String ERROR_ATTRIBUTE = "error";
     private static final String ERROR_MESSAGE = "Can't find any credit cards";
     private static final String CREDIT_CARDS_ATTRIBUTE = "creditCards";
@@ -37,17 +37,7 @@ public class ShowUsersCreditCardsCommand implements Command {
         }
     };
 
-    private static final ResponseContext ERROR_CONTEXT = new ResponseContext() {
-        @Override
-        public String getPage() {
-            return ERROR_PATH;
-        }
-
-        @Override
-        public boolean isRedirect() {
-            return false;
-        }
-    };
+    private static final ResponseContext ERROR_CONTEXT = ErrorResponseContext.getInstance();
 
     public static Command getInstance() {
         return INSTANCE;
@@ -58,7 +48,7 @@ public class ShowUsersCreditCardsCommand implements Command {
 
         HttpSession session;
 
-        if(context.getCurrentSession().isPresent()) {
+        if (context.getCurrentSession().isPresent()) {
             session = context.getCurrentSession().get();
         } else {
             return ERROR_CONTEXT;
