fd2c7d8c2136470b8f667619634ab09191e385c1
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-11-30 00:21:05 +0300

    Refactor code

--- src/main/java/com/epam/jwd/controller/command/impl/ShowUsersCommand.java
+++ src/main/java/com/epam/jwd/controller/command/impl/ShowUsersCommand.java
@@ -1,9 +1,10 @@
 package com.epam.jwd.controller.command.impl;
 
 import com.epam.jwd.controller.command.Command;
+import com.epam.jwd.controller.command.response_context.ErrorResponseContext;
 import com.epam.jwd.controller.command.response_context.ResponseContext;
+import com.epam.jwd.service.impl.user_account.UserService;
 import com.epam.jwd.controller.request_context.RequestContext;
-import com.epam.jwd.service.api.UserService;
 import com.epam.jwd.service.dto.user_account.UserDTO;
 import com.epam.jwd.service.exception.ServiceException;
 import org.apache.logging.log4j.LogManager;
@@ -14,10 +15,9 @@ import java.util.List;
 
 public class ShowUsersCommand implements Command {
 
-    private final UserService<UserDTO, Integer> userService = new com.epam.jwd.service.impl.user_account.UserService();
+    private final UserService userService = new UserService();
     private static final Command INSTANCE = new ShowUsersCommand();
     private static final String PAGE_PATH = "/WEB-INF/jsp/users.jsp";
-    private static final String ERROR_PAGE_PATH = "/WEB-INF/jsp/error.jsp";
     private static final String USERS_ATTRIBUTE = "users";
     private static final String ERROR_ATTRIBUTE = "error";
     private static final String ERROR_MESSAGE = "Can't find users";
@@ -36,17 +36,7 @@ public class ShowUsersCommand implements Command {
         }
     };
 
-    private static final ResponseContext ERROR_CONTEXT = new ResponseContext() {
-        @Override
-        public String getPage() {
-            return ERROR_PAGE_PATH;
-        }
-
-        @Override
-        public boolean isRedirect() {
-            return false;
-        }
-    };
+    private static final ResponseContext ERROR_CONTEXT = ErrorResponseContext.getInstance();
 
     public static Command getInstance() {
         return INSTANCE;
@@ -57,7 +47,7 @@ public class ShowUsersCommand implements Command {
 
         HttpSession session;
 
-        if(context.getCurrentSession().isPresent()) {
+        if (context.getCurrentSession().isPresent()) {
             session = context.getCurrentSession().get();
         } else {
             return ERROR_CONTEXT;
@@ -67,7 +57,6 @@ public class ShowUsersCommand implements Command {
             List<UserDTO> users = userService.findAll();
             session.setAttribute(USERS_ATTRIBUTE, users);
         } catch (ServiceException e) {
-            e.printStackTrace();
             log.error(ERROR_MESSAGE, e);
             context.addAttributeToJsp(ERROR_ATTRIBUTE, ERROR_MESSAGE + e.getMessage());
         }
