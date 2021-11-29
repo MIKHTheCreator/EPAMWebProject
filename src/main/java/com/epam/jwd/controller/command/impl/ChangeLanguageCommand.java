bba3322e2dbfe89e84804035d2660ac850ae7bac
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-11-29 23:17:19 +0300

    Refactor code

--- src/main/java/com/epam/jwd/controller/command/impl/ChangeLanguageCommand.java
+++ src/main/java/com/epam/jwd/controller/command/impl/ChangeLanguageCommand.java
@@ -1,6 +1,7 @@
 package com.epam.jwd.controller.command.impl;
 
 import com.epam.jwd.controller.command.Command;
+import com.epam.jwd.controller.command.response_context.ErrorResponseContext;
 import com.epam.jwd.controller.command.response_context.ResponseContext;
 import com.epam.jwd.controller.request_context.RequestContext;
 
@@ -10,7 +11,6 @@ public class ChangeLanguageCommand implements Command {
 
     private static final Command INSTANCE = new ChangeLanguageCommand();
     private static final String LANGUAGE_ATTRIBUTE = "language";
-    private static final String ERROR_PAGE_PATH = "/WEB-INF/jsp/error.jsp";
     private static final String PAGE_PATH = "/WEB-INF/jsp/main.jsp";
 
     private static final ResponseContext CHANGE_LANGUAGE_CONTEXT = new ResponseContext() {
@@ -25,17 +25,7 @@ public class ChangeLanguageCommand implements Command {
         }
     };
 
-    private static final ResponseContext ERROR_CONTEXT = new ResponseContext() {
-        @Override
-        public String getPage() {
-            return ERROR_PAGE_PATH;
-        }
-
-        @Override
-        public boolean isRedirect() {
-            return false;
-        }
-    };
+    private static final ResponseContext ERROR_CONTEXT = ErrorResponseContext.getInstance();
 
     public static Command getInstance() {
         return INSTANCE;
@@ -47,7 +37,7 @@ public class ChangeLanguageCommand implements Command {
         String language = context.getParameterByName(LANGUAGE_ATTRIBUTE);
 
         HttpSession session;
-        if (context.getCurrentSession().isPresent()){
+        if (context.getCurrentSession().isPresent()) {
             session = context.getCurrentSession().get();
         } else {
             return ERROR_CONTEXT;
