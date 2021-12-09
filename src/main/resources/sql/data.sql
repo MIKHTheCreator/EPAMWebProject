0f5cb4816a8429508a55df430ce1f5d8b045d328
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-12-10 01:55:59 +0300

    Refactor sql scripts

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -2,5 +2,7 @@ INSERT INTO `role` VALUES
     ('ADMIN');
 INSERT INTO `role` VALUES
     ('USER');
-INSERT INTO `role` VALUES
-    ('VISITOR');
+INSERT INTO `client` (`client_id`, `username`, `email`, `password`) VALUES
+    ('1', 'MIKHTheCreator', 'kharevich.ifconfig@gmail.com', 'TWlzaGEzMDA3MjAwMQ==');
+INSERT INTO `user` (`first_name`, `second_name`, `age`, `gender`, `client_id`, `role_id`) VALUES
+    ('ADMIN', 'THEGOD', '1000', 'GOD', '1', '1');
