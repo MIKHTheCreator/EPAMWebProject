fadfd10652651b138c1a55577dbc0ce87260b30c
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-12-10 03:44:42 +0300

    Create CurrencyConverter

--- src/main/java/com/epam/jwd/service/currency_converter/CurrencyConverter.java
+++ src/main/java/com/epam/jwd/service/currency_converter/CurrencyConverter.java
@@ -1,9 +1,16 @@
 package com.epam.jwd.service.currency_converter;
 
+
+import com.epam.jwd.service.exception.ServiceException;
+import com.epam.jwd.service.parser.Parser;
+import com.epam.jwd.service.parser.impl.CurrencyParser;
+
 import java.math.BigDecimal;
 
 public class CurrencyConverter {
 
+    private final Parser parser = CurrencyParser.getInstance();
+    private final RequestSender sender = RequestSender.getInstance();
     private static final CurrencyConverter INSTANCE = new CurrencyConverter();
 
     private CurrencyConverter() {
@@ -13,4 +20,16 @@ public class CurrencyConverter {
         return INSTANCE;
     }
 
+    public BigDecimal convert(String amount, String from, String to)
+            throws ServiceException {
+        String beforeConvert = sender.prepareSendRequest(amount, from, to);
+
+        return convertStringToNumber(beforeConvert);
+    }
+
+    private BigDecimal convertStringToNumber(String number) {
+
+        String result = parser.parse(number);
+        return new BigDecimal(result);
+    }
 }
