53ac3762bca419500131b3e6bee5aba47e5f59ab
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-12-11 14:53:28 +0300

    Refactor code

--- src/main/java/com/epam/jwd/service/config/ValidatorConfig.java
+++ src/main/java/com/epam/jwd/service/config/ValidatorConfig.java
@@ -23,7 +23,7 @@ public interface ValidatorConfig {
     Integer MIN_AGE = 14;
     Integer MAX_AGE = 100;
 
-    String CURRENCY_PATTERN = "^(US|EU|BYN){1}$";
+    String CURRENCY_PATTERN = "^(USD|EUR|BYN){1}$";
     Integer POSITIVE_NUMBER = 0;
 
     String PAYMENT_ORGANIZATION_PATTERN = "^[\\w\\d\\-\\.\\:\\(\\)\\;\\s]{1,45}$";
