afa990b6a416aa6acbef0dc73d95324605f6dd1c
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-12-12 19:48:58 +0300

    Add comments

--- src/main/java/com/epam/jwd/dao/entity/AbstractEntity.java
+++ src/main/java/com/epam/jwd/dao/entity/AbstractEntity.java
@@ -1,13 +1,28 @@
 package com.epam.jwd.dao.entity;
 
-
+/**
+ * @author mikh
+ * Class which AbstractEntity which provides id of provided type
+ * @param <T> type of id
+ */
 public class AbstractEntity<T> {
 
+    /**
+     * Field of generic type which provides id
+     */
     private T id;
 
+    /**
+     * Constructor to create Object of current class
+     * @see AbstractEntity#AbstractEntity(Object)
+     */
     public AbstractEntity() {
     }
 
+    /**
+     * Constructor which creates object with initialized id field {@link AbstractEntity#id}
+     * @param id id to initialize
+     */
     public AbstractEntity(T id) {
         this.id = id;
     }
