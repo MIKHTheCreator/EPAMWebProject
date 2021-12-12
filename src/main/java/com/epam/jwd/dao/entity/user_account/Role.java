211ebc0a20710f04931a2d63f12b0e49a5b442d0
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-12-13 00:46:24 +0300

    Add comments

--- src/main/java/com/epam/jwd/dao/entity/user_account/Role.java
+++ src/main/java/com/epam/jwd/dao/entity/user_account/Role.java
@@ -3,11 +3,20 @@ package com.epam.jwd.dao.entity.user_account;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Enum which describes available roles in Bank System
+ */
 public enum Role {
     UNAUTHORIZED(0), ADMIN(1), USER(2);
 
+    /**
+     * Integer field which describes role id
+     */
     private final Integer roleId;
 
+    /**
+     * List of all available roles of enum
+     */
     private static final List<Role> ALL_AVAILABLE_ROLES = Arrays.asList(values());
 
     Role(Integer roleId) {
