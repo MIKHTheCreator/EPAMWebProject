829bfa859ba17067caa5d25fbeb6930393754fef
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-12-13 00:36:26 +0300

    Add comments

--- src/main/java/com/epam/jwd/dao/entity/user_account/Passport.java
+++ src/main/java/com/epam/jwd/dao/entity/user_account/Passport.java
@@ -5,19 +5,53 @@ import com.epam.jwd.dao.entity.AbstractEntity;
 import java.time.LocalDate;
 import java.util.Objects;
 
+/**
+ * Passport entity class which extends AbstractEntity with Integer id property
+ * This class describes typical passport information
+ */
 public class Passport extends AbstractEntity<Integer> {
 
+    /**
+     * String field with passport's seria and number
+     */
     private String seriaAndNumber;
+    /**
+     * String field with passport's personal number
+     */
     private String personalNumber;
+    /**
+     * LocalDate field with passport's expiration date
+     *
+     * @see LocalDate
+     */
     private LocalDate expirationDate;
 
+    /**
+     * Constructor without arguments for creating empty passport object
+     *
+     * @see Passport#Passport(Integer)
+     * @see Passport#Passport(Integer, String, String, LocalDate)
+     */
     public Passport() {
     }
 
+    /**
+     * Constructor with Integer id argument for creating Passport with current id
+     *
+     * @param id provided id
+     */
     public Passport(Integer id) {
         super(id);
     }
 
+    /**
+     * Constructor with all amount of passport properties for creating whole object
+     *
+     * @param id             passport's id
+     * @param seriaAndNumber passport's seria and number
+     * @param personalNumber person's personal number
+     * @param expirationDate passport's expiration date
+     */
     public Passport(Integer id, String seriaAndNumber, String personalNumber, LocalDate expirationDate) {
         super(id);
         this.seriaAndNumber = seriaAndNumber;
