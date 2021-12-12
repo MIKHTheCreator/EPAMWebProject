61fa3507c68334797441cfe705d91aae46fbef97
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-12-12 19:56:53 +0300

    Add comments

--- src/main/java/com/epam/jwd/dao/entity/payment_system/BankAccount.java
+++ src/main/java/com/epam/jwd/dao/entity/payment_system/BankAccount.java
@@ -5,19 +5,53 @@ import com.epam.jwd.dao.entity.AbstractEntity;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+/**
+ * Bank Account class which extends AbstractEntity class with Integer id field {@link AbstractEntity}
+ * This class provides Bank Account entity with balance, currency fields and shows is this account blocked
+ */
 public class BankAccount extends AbstractEntity<Integer> {
 
+    /**
+     * BigDecimal field which shows bank account balance
+     *
+     * @see BigDecimal
+     */
     private BigDecimal balance;
+    /**
+     * String field which shows bank account currency
+     */
     private String currency;
+    /**
+     * Boolean field which show is current account blocked or not
+     */
     private boolean blocked;
 
+    /**
+     * Constructor without arguments which creates empty object
+     *
+     * @see BankAccount#BankAccount(Integer)
+     * @see BankAccount#BankAccount(Integer, BigDecimal, String, boolean)
+     */
     public BankAccount() {
     }
 
+    /**
+     * Constructor with id argument, which sets id using super class constructor
+     *
+     * @param id Integer id field
+     */
     public BankAccount(Integer id) {
         super(id);
     }
 
+    /**
+     * Constructor with full amount of arguments needed to this object
+     *
+     * @param id       id of the bank account
+     * @param balance  balance of bank account
+     * @param currency currency of bank account
+     * @param blocked  blocked flag
+     */
     public BankAccount(Integer id, BigDecimal balance, String currency, boolean blocked) {
         super(id);
         this.balance = balance;
