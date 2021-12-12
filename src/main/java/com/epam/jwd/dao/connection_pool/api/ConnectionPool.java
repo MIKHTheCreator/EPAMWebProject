5cd53940bf75a6f1716391c314e14c8a2304926d
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-12-12 19:01:29 +0300

    Add comments

--- src/main/java/com/epam/jwd/dao/connection_pool/api/ConnectionPool.java
+++ src/main/java/com/epam/jwd/dao/connection_pool/api/ConnectionPool.java
@@ -4,13 +4,35 @@ import com.epam.jwd.dao.exception.DAOException;
 
 import java.sql.Connection;
 
+/**
+ * @author mikh
+ * <p>
+ * This interface provids connectionPool description with usefull methods which porvide
+ * user with initializing pool, shutdown poll, take and return connections to the pool
+ */
 public interface ConnectionPool {
 
+    /**
+     * Method which create the connection pool with several connections
+     * @throws DAOException if connection can't be accepted or JDBC driver is unavailable
+     */
     void init() throws DAOException;
 
+    /**
+     * Method which close all connections and stops connection pool and opportunity to use connections
+     * @throws DAOException if connection can't be accepted or JDBC driver is unavailable
+     */
     void shutDown() throws DAOException;
 
+    /**
+     * Method which provides user with option to take connection from pool
+     * @return taken connection from pool of class {@link Connection}
+     */
     Connection takeConnection();
 
+    /**
+     * Method which returns taken connection to the pool
+     * @param connection connection to return to pool
+     */
     void returnConnection(Connection connection);
 }
