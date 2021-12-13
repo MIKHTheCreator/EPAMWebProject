4de74b961be1a1d5cac9bbfc63d5a2bdf134e841
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-12-13 22:19:22 +0300

    Refactor code

--- src/main/java/com/epam/jwd/controller/command/response_context/ResponseContext.java
+++ src/main/java/com/epam/jwd/controller/command/response_context/ResponseContext.java
@@ -1,8 +1,19 @@
 package com.epam.jwd.controller.command.response_context;
 
+/**
+ * Interface which provides response context for process method
+ */
 public interface ResponseContext {
 
+    /**
+     * Method that shows current path to the needed page
+     * @return page path
+     */
     String getPage();
 
+    /**
+     * Method which shows if the request should be redirected
+     * @return true if request should be redirected false otherwise
+     */
     boolean isRedirect();
 }
