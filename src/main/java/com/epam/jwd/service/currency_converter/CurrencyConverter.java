f6c38ca1a8eabe3e57a1a715c53318ffb736fd9e
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-12-13 20:42:18 +0300

    Add comments

--- src/main/java/com/epam/jwd/service/currency_converter/CurrencyConverter.java
+++ src/main/java/com/epam/jwd/service/currency_converter/CurrencyConverter.java
@@ -7,9 +7,19 @@ import com.epam.jwd.service.parser.impl.CurrencyParser;
 
 import java.math.BigDecimal;
 
+/**
+ * Currency Converter class which provides user with converting currency into CurrencyEnum Field
+ * Class created by Singleton Pattern
+ */
 public class CurrencyConverter {
 
+    /**
+     * @see Parser
+     */
     private final Parser parser = CurrencyParser.getInstance();
+    /**
+     * @see RequestSender
+     */
     private final RequestSender sender = RequestSender.getInstance();
     private static final CurrencyConverter INSTANCE = new CurrencyConverter();
 
@@ -20,6 +30,15 @@ public class CurrencyConverter {
         return INSTANCE;
     }
 
+    /**
+     * Method for converting input currency amount to another currency
+     *
+     * @param amount input balance
+     * @param from   currency from to convert
+     * @param to     currency to convert to
+     * @return BigDecimal balance in different currency
+     * @throws ServiceException if any service exception was thrown
+     */
     public BigDecimal convert(String amount, String from, String to)
             throws ServiceException {
         String beforeConvert = sender.prepareSendRequest(amount, from, to);
@@ -27,6 +46,12 @@ public class CurrencyConverter {
         return convertStringToNumber(beforeConvert);
     }
 
+    /**
+     * Method for converting String number field to BigDecimal number field
+     *
+     * @param number String balance
+     * @return BigDecimal balance
+     */
     private BigDecimal convertStringToNumber(String number) {
 
         String result = parser.parse(number);
--- src/main/java/com/epam/jwd/service/currency_converter/RequestSender.java
+++ src/main/java/com/epam/jwd/service/currency_converter/RequestSender.java
@@ -12,10 +12,21 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 
+/**
+ * @author mikh
+ * Class which provides user with option of sending the request with parameters to currency converter web-site
+ * Class created by Singleton Pattern
+ */
 public class RequestSender {
 
+    /**
+     * @see CloseableHttpClient
+     */
     private final CloseableHttpClient httpClient = HttpClients.createDefault();
     private static final RequestSender INSTANCE = new RequestSender();
+    /**
+     * String Url for sending a request
+     */
     private static final String XE_CONVERTER_URL = "https://www.xe.com/currencyconverter/convert/?Amount=[amount]&From=[from]&To=[to]";
     private static final String ERROR_MESSAGE = "IOException caught";
 
@@ -28,6 +39,15 @@ public class RequestSender {
         return INSTANCE;
     }
 
+    /**
+     * Method which rebuild seb-site URL with needed currency and amount parameters
+     *
+     * @param amount amount parameter
+     * @param from   currency from to convert
+     * @param to     currency to convert
+     * @return String prepared URL
+     * @throws ServiceException if it was unable to send request
+     */
     public String prepareSendRequest(String amount, String from, String to)
             throws ServiceException {
 
