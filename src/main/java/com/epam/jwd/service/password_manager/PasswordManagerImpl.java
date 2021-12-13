a1f21427faf29cdf4eb0188d5d3f04918c5e6094
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-12-13 21:33:49 +0300

    Add comments

--- src/main/java/com/epam/jwd/service/password_manager/PasswordManagerImpl.java
+++ src/main/java/com/epam/jwd/service/password_manager/PasswordManagerImpl.java
@@ -5,14 +5,24 @@ import org.apache.xerces.impl.dv.util.Base64;
 
 import java.nio.charset.StandardCharsets;
 
-
+/**
+ * PasswordManager Class which implements PasswordManager and provides methods for work with Base64 encoder
+ * @author mikh
+ * @see PasswordManager
+ */
 public class PasswordManagerImpl implements PasswordManager {
 
+    /**
+     * @see PasswordManager#encode(String)
+     */
     @Override
     public String encode(String password) {
         return Base64.encode(password.getBytes(StandardCharsets.UTF_8));
     }
 
+    /**
+     * @see PasswordManager#decode(String)
+     */
     @Override
     public String decode(String password) {
         StringBuilder result = new StringBuilder();
@@ -22,6 +32,9 @@ public class PasswordManagerImpl implements PasswordManager {
         return result.toString();
     }
 
+    /**
+     * @see PasswordManager#checkForIdentity(String, String)
+     */
     @Override
     public boolean checkForIdentity(String inputPassword, String encodedPassword) {
         return inputPassword.equals(encodedPassword);
--- src/main/java/com/epam/jwd/service/password_manager/api/PasswordManager.java
+++ src/main/java/com/epam/jwd/service/password_manager/api/PasswordManager.java
@@ -1,10 +1,34 @@
 package com.epam.jwd.service.password_manager.api;
 
+/**
+ * Interface that provides methods to encode and decode strings
+ *
+ * @author mikh
+ */
 public interface PasswordManager {
 
+    /**
+     * Method which encode string using Base64 algorithms
+     *
+     * @param password string to encode
+     * @return encoded string
+     */
     String encode(String password);
 
+    /**
+     * Method for decoding inputed string
+     *
+     * @param password string to decode
+     * @return decoded string
+     */
     String decode(String password);
 
+    /**
+     * Method for checking to strings for identity
+     *
+     * @param firstString  decoded string
+     * @param secondString encoded string
+     * @return true if strings are equals false otherwise
+     */
     boolean checkForIdentity(String firstString, String secondString);
 }
