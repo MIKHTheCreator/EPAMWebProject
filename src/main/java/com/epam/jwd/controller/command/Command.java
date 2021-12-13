a669d0f768c72cec014b82e20d8b5332d61883e1
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-12-13 22:16:28 +0300

    Refactor code

--- src/main/java/com/epam/jwd/controller/command/Command.java
+++ src/main/java/com/epam/jwd/controller/command/Command.java
@@ -3,10 +3,25 @@ package com.epam.jwd.controller.command;
 import com.epam.jwd.controller.command.response_context.ResponseContext;
 import com.epam.jwd.controller.request_context.RequestContext;
 
+/**
+ * Interface which provides command pattern methods for classes
+ */
 public interface Command {
 
+    /**
+     * Method which executes current command
+     *
+     * @param context {@link RequestContext}
+     * @return {@link RequestContext}
+     */
     ResponseContext execute(RequestContext context);
 
+    /**
+     * Method which gets command by its name from all amount of available commands
+     *
+     * @param commandName command name
+     * @return command
+     */
     static Command of(String commandName) {
         return Commands.of(commandName);
     }
