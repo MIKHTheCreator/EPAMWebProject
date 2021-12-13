a627af3664b1ed6c8deb746305dfb68731d00d8a
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-12-13 21:38:45 +0300

    Add comments

--- src/main/java/com/epam/jwd/service/payment_manager/PaymentManager.java
+++ src/main/java/com/epam/jwd/service/payment_manager/PaymentManager.java
@@ -10,6 +10,12 @@ import static com.epam.jwd.dao.message.ExceptionMessage.DELIMITER;
 import static com.epam.jwd.service.message.ExceptionMessage.NOT_ENOUGH_MONEY_TO_PAY_EXCEPTION;
 import static com.epam.jwd.service.message.ExceptionMessage.NOT_ENOUGH_MONEY_TO_PAY_EXCEPTION_CODE;
 
+/**
+ * Class for managing payments
+ * Class created as Singleton
+ *
+ * @author mikh
+ */
 public class PaymentManager {
 
     private final BankAccountCashValidator validator = BankAccountCashValidator.getInstance();
@@ -26,21 +32,49 @@ public class PaymentManager {
         return INSTANCE;
     }
 
+    /**
+     * Method for getting payment goal as a number
+     *
+     * @param goal string payment goal
+     * @return id of goal operation
+     */
     private int checkForPaymentGoal(String goal) {
         String convertedGoal = paymentGoalConverter(goal);
         return PaymentGoal.getOperationIdByName(convertedGoal);
     }
 
+    /**
+     * Method for converting input goal to the needed format
+     *
+     * @param inputGoal unconverted goal
+     * @return converted goal as a String
+     */
     private String paymentGoalConverter(String inputGoal) {
         return inputGoal.replace(SPACE_CHAR, UNDERSCORE).toUpperCase();
     }
 
+    /**
+     * Method for checking is it enough money to pay
+     *
+     * @param sumOfPayment sum of payment
+     * @param currentCash  current bank account cash
+     * @throws ServiceException if it's not enough money to pay
+     */
     private void isEnoughMoney(BigDecimal sumOfPayment, BigDecimal currentCash) throws ServiceException {
         if (!validator.isEnoughCashForPayment(sumOfPayment, currentCash)) {
             throw new ServiceException(NOT_ENOUGH_MONEY_TO_PAY_EXCEPTION + DELIMITER + NOT_ENOUGH_MONEY_TO_PAY_EXCEPTION_CODE);
         }
     }
 
+    /**
+     * Method for choosing input operation
+     *
+     * @param goal         payment goal
+     * @param sumOfPayment sum of payment
+     * @param currentCash  current bank account cash
+     * @return operation number(0-add operation, 1-subtract operation)
+     * @throws ServiceException if it's not enough money to pay
+     */
     public Integer chooseOperation(String goal, BigDecimal sumOfPayment, BigDecimal currentCash) throws ServiceException {
         if (checkForPaymentGoal(goal) == SUBTRACT_OPERATION_NUMBER) {
             isEnoughMoney(sumOfPayment, currentCash);
